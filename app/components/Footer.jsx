cc5be5d96aa19d8e745daee7918092af37279b2a
Shubham-Thakur999 <130841476+Shubham-Thakur999@users.noreply.github.com>
2025-03-13 12:46:51 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
         </div>
 
         <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-            <p>© 2025 Shubham Takur. All rights reserved.</p>
+            <p>© 2025 Shubham Thakur. All rights reserved.</p>
             <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
                 <li><a target='_blank' href="https://github.com/Shubham-Thakur999">Github</a></li>
                 <li><a target='_blank' href="www.linkedin.com/in/shubham-thakur-01286422a">LinkedIn</a></li>
